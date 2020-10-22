031cae8b95c925ea74503e94d783b0a71bfa926c
Alex Bochman <alexbochman22@gmail.com>
2020-10-21 23:11:32 -0400

    Restructured directories

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var x = 10;
+var y = 20;
+var add = function(a, b) { return a + b; }
+
+add(x,y);
---
+++ ~Homework/HW5/js/Homework5.js
@@ -0,0 +1,17 @@
+
+function generateTable() {
+    var xStart = document.getElementById('xStart').value;
+    var xEnd = document.getElementById('xEnd').value;
+    var yStart = document.getElementById('yStart').value;
+    var yEnd = document.getElementById('yEnd').value;
+    console.log(xStart);
+    console.log(xEnd);
+    console.log(yStart);
+    console.log(yEnd);
+    
+    var test = document.getElementById('test');
+    test.innerHTML = xStart + " " + xEnd + " " + yStart + " " + yEnd;
+    
+}
+
+
